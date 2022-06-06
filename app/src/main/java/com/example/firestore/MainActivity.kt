fef6e62a3069fc1c5549bf73f87a99685c1135b2
Tanmay <tanmaysingh0311@gmail.com>
2022-06-06 08:24:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firestore/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.firestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
