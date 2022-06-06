66c6c9726951b1767cab802294585d404d10f30f
Tanmay <tanmaysingh0311@gmail.com>
2022-06-06 14:31:59 +0530

    add comment

--- app/src/main/java/com/example/firestore/MainActivity.kt
+++ app/src/main/java/com/example/firestore/MainActivity.kt
@@ -14,7 +14,15 @@ import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+//    Firestore stores data in collection of documents.
+//    Means we start with collections .
+//    That contains documents.
+//    First we will get a reference to collection
+
+    //take out instance of firestore database
     private val fireStoreDb = Firebase.firestore
+
+    //take out particular collection reference
     private val personCollectionRef = fireStoreDb.collection("persons")
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,17 +36,17 @@ class MainActivity : AppCompatActivity() {
 
         btnUploadData.setOnClickListener{
             val firstName = etFirstName.text.toString()
-val lastName =  etLastName.text.toString()
+            val lastName =  etLastName.text.toString()
             val age =  etAge.text.toString()
             val person = Person(firstName, lastName, age)
             savePerson(person)
         }
-
-
-
     }
+
     private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
         try {
+
+            //await() is used to wait thread till uploading data is finished
             personCollectionRef.add(person).await()
             withContext(Dispatchers.Main){
                 Toast.makeText(this@MainActivity, "Data Successfully saved", Toast.LENGTH_SHORT).show()
